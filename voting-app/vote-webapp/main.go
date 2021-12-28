6c6a85f34b5e1b3e8ca8ca4faa76706360f83a03
hbollon <hugo.bollon@gmail.com>
2021-12-28 23:37:21 +0100

    fix: gorilla/mux issues with Vue

--- voting-app/vote-webapp/main.go
+++ voting-app/vote-webapp/main.go
@@ -8,6 +8,8 @@ import (
 	"io/ioutil"
 	"math/rand"
 	"net/http"
+	"os"
+	"path/filepath"
 	"strconv"
 	"time"
 
@@ -117,8 +119,8 @@ func main() {
 	apiRouter.HandleFunc("/vote", handleWithRedis(submitVoteHandler, &Redis{red}))
 
 	// Serve static files (CSS, JS, images) from dir
-	fs := http.FileServer(http.Dir("/static"))
-	r.PathPrefix("/").Handler(fs)
+	spa := spaHandler{staticPath: "static", indexPath: "index.html"}
+	r.PathPrefix("/").Handler(spa)
 
 	// Add CORS Middleware to mux router
 	r.Use(corsMiddleware)
@@ -127,4 +129,47 @@ func main() {
 	port := 80
 	log.Infof("Listening on port %d\n", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), r))
+}
+
+// spaHandler implements the http.Handler interface, so we can use it
+// to respond to HTTP requests. The path to the static directory and
+// path to the index file within that static directory are used to
+// serve the SPA in the given static directory.
+type spaHandler struct {
+	staticPath string
+	indexPath  string
+}
+
+// ServeHTTP inspects the URL path to locate a file within the static dir
+// on the SPA handler. If a file is found, it will be served. If not, the
+// file located at the index path on the SPA handler will be served. This
+// is suitable behavior for serving an SPA (single page application).
+func (h spaHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// get the absolute path to prevent directory traversal
+	path, err := filepath.Abs(r.URL.Path)
+	if err != nil {
+		// if we failed to get the absolute path respond with a 400 bad request
+		// and stop
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// prepend the path with the path to the static directory
+	path = filepath.Join(h.staticPath, path)
+
+	// check whether a file exists at the given path
+	_, err = os.Stat(path)
+	if os.IsNotExist(err) {
+		// file does not exist, serve index.html
+		http.ServeFile(w, r, filepath.Join(h.staticPath, h.indexPath))
+		return
+	} else if err != nil {
+		// if we got an error (that wasn't that the file doesn't exist) stating the
+		// file, return a 500 internal server error and stop
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// otherwise, use http.FileServer to serve the static dir
+	http.FileServer(http.Dir(h.staticPath)).ServeHTTP(w, r)
 }
